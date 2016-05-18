48d0a48f86829d6df79eb5f55955db3eb657e88d
Denis Belyaev <belyaev@playform.com>
2016-05-18 10:18:09 +0300

    add api params sorting

--- src/app/api.js
+++ src/app/api.js
@@ -1,32 +1,60 @@
 var wordApi = wordApi || {};
 
 wordApi = function(game) {
-    var _this = this;
-    var CLIENT_KEY = '8K2rxSUn7xtU21j2pnxp';
-    var sid = '';
+    var _this = this,
+        CLIENT_KEY = '8K2rxSUn7xtU21j2pnxp',
+        sid = '',
+        viewer_id = '12',
+        viewer_platform = 0;
+
+
     return {
         init: function() {
 
         },
+        prepareData: function(params) {
+            var self = this;
+            params.viewer_id = viewer_id;
+            params.viewer_platform = viewer_platform;
+            params.name = 'sderxc';
+            function ksort(w) {
+                var sArr = [], tArr = [], n = 0;
+
+                for (i in w){
+                    tArr[n++] = i;
+                }
+
+                tArr = tArr.sort();
+                for (var i=0, n = tArr.length; i<n; i++) {
+                    sArr[tArr[i]] = w[tArr[i]];
+                }
+                return sArr;
+            }
+            //console.log(ksort(params));
+            //return self.prepareSig(params);
+            return self.prepareSig(ksort(params));
+        },
         prepareSig: function(params) {
-            var str = '';
+            var str = viewer_id;
             for (var key in params) {
-                str+= key+params[key];
+                str+= key+'='+params[key];
             }
             str+=CLIENT_KEY+sid;
+            console.log(str);
 
             params.sig = md5(str);
             return params;
         },
         updateSid: function(answer){
-            //TODO добавить обработку __new_sid
+            //TODO __new_sid
             if (answer.__sid) {
                 sid = answer.__sid;
             }
         },
         send: function(params, successCb, errorCb) {
             var self = this;
-            params.data = this.prepareSig(params.data);
+            params.data = self.prepareData(params.data);
+            console.log(params.data);
             $.post(params.url, params.data, function(data){
                 if (data.data) {
                     successCb(data.data);
